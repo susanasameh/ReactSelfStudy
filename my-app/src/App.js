86246f86edadb90e0b4d4e87544543e68d8adfe4
susanasameh <susana.sameh.2692@gmail.com>
2024-10-29 12:26:05 +0300

    importImage& inlineStyling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,24 @@
+// import reactLogo from './reactGirl.jpg';
+
 function App() {
     return (
-        <div>
-            <h1>Iam</h1>
-            <h2>Susana</h2>
-            <h3>Web Developer</h3>
+      <div id="meData">
+        {/* how to upload img in react */}
+        {/* <img src={reactLogo} alt="reactLogo" id="reactlogo" /> */}
+
+        <img
+          src={require("./reactGirl.jpg")}
+          alt="reactLogo"
+          class="reactlogo"
+        />
+     
+
+        <div id="meDataContent">
+          <h1>Iam</h1>
+          <h2>Susana</h2>
+          <h3>Web Developer</h3>
         </div>
+      </div>
     );
 }
 export default App;
--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,13 +1,38 @@
 function Header() {
     return (
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-                <li><a href="/career">Career</a></li>
-            </ul>
-        </nav>
+      <nav>
+        <ul id="navUl">
+          {/* get img from net */}
+          <img
+            src="https://img.freepik.com/free-vector/hand-drawn-flat-design-devops-illustration_23-2149375793.jpg?t=st=1730191583~exp=1730195183~hmac=89f434b4f3d10c3ffa9a706c8156c937185f0965a805a5866039182605d11e9e&w=740"
+            alt="reactLogo2"
+            style = {{ width : "100px", marginRight: "30px", marginTop : "", height: "100px"}}
+                    
+          />
+                <div style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    listStyleType: "none",
+                    padding: "0",
+                    margin: "0",
+                    width: "50%",
+          }}>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <a href="/career">Career</a>
+            </li>
+          </div>
+        </ul>
+      </nav>
     );
 };
 export default Header;
