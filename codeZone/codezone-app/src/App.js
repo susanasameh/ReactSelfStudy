ad71eab911e30891921d689efb1973361676ad76
susanasameh <susana.sameh.2692@gmail.com>
2024-11-24 19:47:46 +0200

    jsx example

--- codeZone/codezone-app/src/App.js
+++ codeZone/codezone-app/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 
 function App() {
-  return(
- <div><h1>hello</h1></div>
+  //----------------- jsx example----------------------------------
+  const profileLink = "https://www.linkedin.com/in/susana-sameh";
+  const myName = 'Susana Sameh';
+  
+  function greeting(name) {
+    if (name) {
+      return (
+        <h2>
+          Hello ,<a className="profile-link" href={profileLink}>{name}</a>
+        </h2>
+      );
+    }
+    return <h2>Hello, Guest</h2>
+  }
+  return (
+    <div className="App">
+      <h1>{greeting(myName)}</h1>
+     
+    </div>
   );
 }
 
